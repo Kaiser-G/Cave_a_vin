65f6e21f8b7efa0a904a2a2c0e5c70250ab64988
Kaiser-G <kaiserg6969@gmail.com>
2024-03-20 17:55:04 +0100

    Ajout des images en fond

--- GestionDesTables/Js/init.js
+++ GestionDesTables/Js/init.js
@@ -1,7 +1,1 @@
-//const url = "http://fbrc.esy.es/DWWM22239/Api/api.php";
-//rajouter a la ligne get(url + "/....", GetAjax, erreur)
-//function afficher_Liste() {
-//   get(url + "/PAYS", GetAjax, erreur);
-// }
-//changer url par :
 const url = "https://afpafabrice.space/DWWM23316/Api/api.php";
